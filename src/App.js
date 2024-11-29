ee2dca6c650e80a6327ff9b8fb358a706fe77c90
Jossue Toscano <jatoscano6@espe.edu.ec>
2024-11-29 12:05:54 -0500

    cambio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
+/* import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
@@ -22,4 +23,41 @@ function App() {
   );
 }
 
-export default App;
+export default App; */
+
+import React from "react";
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+function App(){
+
+  const[asientos, setAsientos]=useState([]);
+
+  //Backend
+
+  const obtenerAsientos = async()=>{
+    try{
+      const respuesta= await axios.get('http://localhost:3000/api/asientos');
+      setAsientos(respuesta.data);
+
+    } catch(error){
+      console.error('Error al capturar los asientos:', error)
+
+    }
+  }
+  const reservarAsientos = async(id)=>{
+    try{
+      const respuesta= await axios.post(`http://localhost:3000/api/asientos/${id}`);
+      setAsientos(respuesta.data);
+
+    } catch(error){
+      console.error('Error al capturar los asientos:', error)
+
+    }
+  }
+  return(
+    <div>
+
+    </div>
+  ); 
+}
